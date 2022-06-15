5b84c6ed8f0ecd884a6ad5f38e6309313f8bd5ca
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-06-15 20:11:28 +0100

    normalize email address

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,3 +17,22 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test email is normalized for new users."""
+
+        sample_emails = [
+            ['test1@EXAMple.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TeSt3@ExaMple.com', 'TeSt3@example.com'],
+            ['TESt3@ExaMple.com', 'TESt3@example.com'],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_use_without_email_raises_error(self):
+        """" Test that creating a user without an email raise a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
