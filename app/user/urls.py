f6e46d1583a7d4c607744829a8e14d8951b91dac
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-06-17 19:02:07 +0100

    Implement create user API

---
+++ app/user/urls.py
@@ -0,0 +1,12 @@
+"""
+URL mapping for the user API
+"""
+
+from django.urls import path
+from .views import CreateUserView
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/',  CreateUserView.as_view(), name='create')
+]
