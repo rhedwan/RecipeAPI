90851b76739999ee2a81687b9fb51837ec1d99ee
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-06-17 17:24:58 +0100

    add the spectacular to url and settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -133,5 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFUALT_SCHEMA_CLASS':'drf_spectacular.openapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
