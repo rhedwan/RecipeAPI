a862d74176943a63a87b1ba42a19dd06761d62e1
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-06-29 15:56:24 +0100

    implement the tag views update

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -15,6 +15,11 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """Create and return a tag detail url"""
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_user(email='test@example.com', password='linger_strike'):
     """Create a new user """
     return get_user_model().objects.create_user(email, password)
@@ -65,3 +70,15 @@ class PrivateTagsAPITests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+
+    def test_update_tag(self):
+        """Test updating tag"""
+
+        tag = Tag.objects.create(user=self.user, name='After dinner')
+        payload = {'name': 'desert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
