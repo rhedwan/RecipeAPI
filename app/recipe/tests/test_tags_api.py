5bb3059c34cb4a20c6c01f6d29ed9c6a0e168bce
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-06-29 16:04:07 +0100

    implement the tag views delete && test

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -71,7 +71,6 @@ class PrivateTagsAPITests(TestCase):
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
 
-
     def test_update_tag(self):
         """Test updating tag"""
 
@@ -82,3 +81,15 @@ class PrivateTagsAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         tag.refresh_from_db()
         self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """Test Deleting a tag """
+
+        tag = Tag.objects.create(user=self.user, name='After drink')
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
+
